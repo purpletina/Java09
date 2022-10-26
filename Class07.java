90de1c57734cc632761ddc42758f80bb3346307c
purpletina <113041666+purpletina@users.noreply.github.com>
2022-10-27 01:22:22 +0800

    Create Class07.java

---
+++ Class07.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package Java09;
+
+/**
+ * @author user
+ *
+ */
+class CCCount{
+	public static void ppp(int i,int j) {
+		System.out.println(Math.pow(i, j));
+	}
+}
+public class J07 {
+	public static void main(String[] args) {
+		CCCount.ppp(2, 5);
+		CCCount.ppp(3, 2);
+
+	}
+
+}
